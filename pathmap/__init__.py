325d652c0c4b922af7111f2a10788d23bc746c64
Steve Peak <steve@stevepeak.net>
2017-07-25 11:17:10 +0200

    v++

--- pathmap/__init__.py
+++ pathmap/__init__.py
@@ -14,4 +14,4 @@ from .utils import (
 from .tree import Tree
 
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ test_requirements = [
 
 setup(
     name='pathmap',
-    version='0.1.3',
+    version='0.1.4',
     description="Path resolver",
     author="Thordur Hermannsson",
     author_email='doddi@kott.is',
